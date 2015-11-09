d77712f1b3be2fab1f3aa8999eb2addab6c3769f
AbelSu <abel@abeldemacbook-pro.local>
2015-11-09 23:37:47 +0800

    Initial Commit

---
+++ PodAbelsu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodAbelsu
+//
+//  Created by abel on 15/11/9.
+//  Copyright © 2015年 abel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PodAbelsu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PodAbelsu
+//
+//  Created by abel on 15/11/9.
+//  Copyright © 2015年 abel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
